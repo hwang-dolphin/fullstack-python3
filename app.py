1b32ad849b6f829221189e4a824df3877980ca59
Robert <robert@Robertos-MacBook-Pro.local>
2021-07-11 22:59:05 -0700

    reactjs

--- app.py
+++ app.py
@@ -2,11 +2,12 @@
 from flask import (Flask, render_template)
 
 app = Flask(__name__)
+app = Flask(__name__, static_folder='../build', static_url_path='/')
 
 @app.route("/")
 def hello():
-    return render_template("index.html")
-
+    #return render_template("index.html")
+	return app.send_static_file('index.html')
 
 @app.route("/blog")
 def get_blog():
